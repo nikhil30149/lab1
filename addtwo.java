889ceb02475caf768593ecdad9bc3d8c1b55cbae
nikhil30149 <59112361+nikhil30149@users.noreply.github.com>
2019-12-28 14:59:33 +0530

    Add files via upload

---
+++ addtwo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Addtwo 
+{
+
+    public static void main(String[] args) 
+{
+        
+        int num1, num2, sum;
+        num1 = 100;
+        num2 30;
+        sum = num1 + num2;
+        System.out.println("Sum of these numbers: "+sum);
+    }
+}
